b1b42117d6c1c057c9af2f177c8a1849ad096472
Charles Clemens <cclemensjr@gmail.com>
2018-10-15 22:45:00 -0700

    Passed Challenge 5

--- code-challenges/301/for-each/for-each.test.js
+++ code-challenges/301/for-each/for-each.test.js
@@ -71,6 +71,11 @@ This anonymous function should accept up to three arguments: the element, the in
 
 const removeWithAnon = (arr) => {
   // Solution code here...
+  arr.forEach((element, index, arr) => {
+    (element % 3 === 2) ? arr.pop() : console.log('No remainder of 2');
+  });
+
+  return arr;
 }
 
 /* ------------------------------------------------------------------------------------------------
@@ -140,7 +145,7 @@ describe('Testing challenge 4', () => {
   });
 });
 
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   test('It should remove three elements from the array', () => {
     expect(removeWithAnon([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toStrictEqual([1, 2, 3, 4, 5, 6, 7]);
     expect(removeWithAnon([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).length).toStrictEqual(7);
