2d11d7b6187cf18866d6cefa0e276f8e400da64f
Charles Clemens <cclemensjr@gmail.com>
2018-10-25 07:10:35 -0700

    Refactored challenge 1 solution

--- code-challenges/301/filter/filter.test.js
+++ code-challenges/301/filter/filter.test.js
@@ -6,10 +6,7 @@ Write a function named oddValues that, given an array of integers as input, uses
 For example, oddValues([1,2,3]) returns [1,3].
 ------------------------------------------------------------------------------------------------ */
 
-const oddValues = (arr) => {
-  // Solution code here...
-  return arr.filter(val => val % 2 !== 0);
-};
+const oddValues = (arr) => arr.filter(val => val % 2 !== 0);
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
