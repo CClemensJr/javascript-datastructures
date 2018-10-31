00e4e4afed0603c95bb46174b88187a6edea2816
Charles Clemens <cclemensjr@gmail.com>
2018-10-31 08:53:39 -0700

    Passed challenge 1

--- code-challenges/301/nested-for-loops/nested-for-loops.test.js
+++ code-challenges/301/nested-for-loops/nested-for-loops.test.js
@@ -10,28 +10,28 @@ Return the array of the total number of cookies sold per hour for all of the sto
 
 const hoursOpen = ['9 a.m.', '10 a.m.', '11 a.m.', '12 p.m.', '1 p.m.', '2 p.m.', '3 p.m.', '4 p.m.', '5 p.m.', '6 p.m.', '7 p.m.', '8 p.m.'];
 
-const firstPike = [17, 18, 23, 24, 24, 12, 13, 27, 30, 20, 24, 18];
-const seaTac = [26, 5, 5, 59, 23, 39, 38, 20, 30, 7, 59, 43];
+const firstPike =     [17, 18, 23, 24, 24, 12, 13, 27, 30, 20, 24, 18];
+const seaTac =        [26, 5, 5, 59, 23, 39, 38, 20, 30, 7, 59, 43];
 const seattleCenter = [7, 14, 19, 22, 15, 4, 23, 27, 28, 23, 1, 29];
-const capHill = [5, 85, 58, 51, 50, 13, 33, 32, 47, 94, 31, 62];
-const alkiBeach = [33, 31, 147, 130, 27, 93, 38, 126, 141, 63, 46, 17];
+const capHill =       [5, 85, 58, 51, 50, 13, 33, 32, 47, 94, 31, 62];
+const alkiBeach =     [33, 31, 147, 130, 27, 93, 38, 126, 141, 63, 46, 17];
 
 const cookieStores = [firstPike, seaTac, seattleCenter, capHill, alkiBeach];
 
 const grandTotal = (stores) => {
   const totalCookies = [];
-  // Solution code here...
-  for (let i = 0; i < stores.length; i++) {
-    console.log(stores[i][i]);
+  
+  for (let i = 0; i < hoursOpen.length; i++) {
     let sum = 0;
-    
-    for (let j = 0; j < stores[i].length; j++) {
-      console.log(stores[i][j]);
-      sum += stores[i][j];
-      totalCookies.push(sum);
+
+    for (let j = 0; j < stores.length; j++) {
+      sum += stores[j][i];
+      
     }
+    totalCookies.push(sum);
   }
-  console.log(totalCookies);
+
+  return totalCookies;
 }
 
 /* ------------------------------------------------------------------------------------------------
@@ -148,7 +148,7 @@ Run your tests from the console: jest challenge-12.test.js
 ------------------------------------------------------------------------------------------------ */
 
 
-xdescribe('Testing challenge 1', () => {
+describe('Testing challenge 1', () => {
   test('It should add the hourly totals array', () => {
     expect(grandTotal(cookieStores)).toStrictEqual([88, 153, 252, 286, 139, 161, 145, 232, 276, 207, 161, 169]);
   });
