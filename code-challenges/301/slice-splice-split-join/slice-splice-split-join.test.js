7c32a5f5ff2cdf433609c70b53ab588d27235557
Charles Clemens <cclemensjr@gmail.com>
2018-11-04 20:46:08 -0800

    Attempted Challenge 6

--- code-challenges/301/slice-splice-split-join/slice-splice-split-join.test.js
+++ code-challenges/301/slice-splice-split-join/slice-splice-split-join.test.js
@@ -129,7 +129,14 @@ For example:
 ------------------------------------------------------------------------------------------------ */
 
 const removeEvenValues = (arr) => {
-  // Solution code here...
+  for(let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      arr.splice(i, 1)
+
+    }
+  }
+
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -226,7 +233,7 @@ describe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should remove the even numbers from the array', () => {
     let list = [1, 2, 3, 4, 5, 6];
     removeEvenValues(list);
